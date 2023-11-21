80279724cef2515d35d939891c55b030cfb5e536
Saurabh Sain <visual.sain100486@gmail.com>
2023-11-21 12:33:07 +0530

    sum

--- arrayOprater01.dart
+++ arrayOprater01.dart
@@ -1,4 +1,5 @@
 import 'dart:ffi';
+import 'dart:svg';
 
 void main() {
   arraySum();
@@ -7,7 +8,7 @@ void main() {
 // Implement a function to find the sum of elements in an array.=?
 
 arraySum() {
-  List Number = [
+  List Numbers = [
     23,
     45,
     46,
@@ -16,13 +17,22 @@ arraySum() {
     57,
     90,
   ];
+
   // For methode
   // var sum = 0.0;
   // for (var i = 0; i < Number.length; i++) {
   //   sum += Number[i];
   // }
   // print(sum);
+
   // reduse methode
-  var sum = Number.reduce((a, b) => a + b);
+  // var sum = Number.reduce((a, b) => a + b);
+  // print(sum);
+
+  // for-each loop methode
+  var sum = 0;
+  Numbers.forEach((Number) {
+    sum += Number;
+  });
   print(sum);
 }
